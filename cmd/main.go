667e16b78fc18a3804adc034ccd7ca76841f77ad
wright <wright.huang@baiyitech.com.tw>
2024-04-15 19:34:30 +0900

    feat: add connection handle

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -18,7 +19,24 @@ type ServerStats struct {
 	People int
 }
 
-var stats = ServerStats{}
+var Stats = ServerStats{}
+
+type Room struct {
+	UserIds []int
+	Msgs    []string
+}
+
+var room1 = Room{}
+
+var NowUserIncr int64 = 0
+
+type UserConnsStruct struct {
+	Lock    sync.Mutex
+	ConnMap map[int]*websocket.Conn
+}
+
+var UserConns = UserConnsStruct{}
+var UserIncr = 0
 
 func main() {
 
@@ -26,19 +44,35 @@ func main() {
 
 	router := gin.Default()
 	router.GET("/ws", func(c *gin.Context) {
-		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+
+		// 升級連線
+		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 		if err != nil {
 			return
 		}
+		defer ws.Close()
+
+		// 連線處理
+		userId := registerConn(ws)
+		defer unregisterConn(userId)
 
 		// 增加人數
 		addPeople()
-
-		defer conn.Close()
 		defer subPeople()
 
-		sendText(conn, "hello!")
+		// 房間人數增加
+		room1.UserIds = append(room1.UserIds, userId)
+
+		sendText(ws, "hello!")
 		for {
+			_, textByte, err := ws.ReadMessage()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			text := string(textByte)
+			fmt.Println(text)
 		}
 	})
 
@@ -50,16 +84,35 @@ func sendText(conn *websocket.Conn, msg string) {
 }
 
 func subPeople() {
-	stats.People--
+	Stats.People--
 }
 
 func addPeople() {
-	stats.People++
+	Stats.People++
 }
 
 func showPeople() {
 	for {
-		fmt.Println("目前有 " + strconv.Itoa(stats.People) + " 人")
+		fmt.Println("目前有 " + strconv.Itoa(Stats.People) + " 人")
+		fmt.Println(UserConns.ConnMap)
 		time.Sleep(time.Second)
+	}
+}
+
+func registerConn(c *websocket.Conn) int {
+	UserConns.Lock.Lock()
+
+	if UserConns.ConnMap == nil {
+		UserConns.ConnMap = make(map[int]*websocket.Conn)
+	}
+	UserIncr++
+	UserConns.ConnMap[UserIncr] = c
+	UserConns.Lock.Unlock()
+	return UserIncr
+}
+
+func unregisterConn(userId int) {
+	if UserConns.ConnMap != nil {
+		delete(UserConns.ConnMap, userId)
 	}
 }
